b2e3b05620eca00258f3cb978285d3e17e424b68
ohill17 <130096459+ohill17@users.noreply.github.com>
2023-10-04 21:21:36 -0700

    Delete AllAboutRiversCuomo directory

--- AllAboutRiversCuomo/AllAboutRiversCuomo/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace AllAboutRiversCuomo.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
